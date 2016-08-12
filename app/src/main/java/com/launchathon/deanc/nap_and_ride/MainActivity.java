cb3b2229251d636736c05252673778257734f56c
emerani <ezraerani@gmail.com>
2016-08-12 14:33:57 -0400

    resolve dependencies

--- app/src/main/java/com/launchathon/deanc/nap_and_ride/MainActivity.java
+++ app/src/main/java/com/launchathon/deanc/nap_and_ride/MainActivity.java
@@ -1,8 +1,14 @@
 package com.launchathon.deanc.nap_and_ride;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -14,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     @Bind(R.id.lat) EditText lat;
     @Bind(R.id.lon) EditText lon;
     @Bind(R.id.wakeup_distance) EditText wake_up;
+    NotificationManager manager;
 
 
 
@@ -21,18 +28,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        manager = ((NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE));
 
 
         ButterKnife.bind(this);
+
+
 
 
     }
 
     @OnClick(R.id.submitButton)
     public void setStop() {
-        String a = lat.getText().toString();
-        String b = lon.getText().toString();
+        Long latitude = Long.valueOf(lat.getText().toString());
+        Long longituge = Long.valueOf(lon.getText().toString());
+        Float radius = Float.valueOf(wake_up.getText().toString());
 
+        Geofence.Builder builder = new Geofence.Builder();
+        builder.setCircularRegion(latitude, longituge, radius)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER);
+        Geofence geofence = builder.build();
+        getGeofencingRequest(geofence);
+
+
+
+    }
+
+    private GeofencingRequest getGeofencingRequest(Geofence geofence) {
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+        builder.addGeofence(geofence);
+        return builder.build();
+    }
+
+    private void triggerNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        builder.setContentTitle("arrived");
+        manager.notify(0, builder.build());
     }
 }
