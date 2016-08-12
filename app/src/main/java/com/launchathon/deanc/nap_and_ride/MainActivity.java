9fed3feea7b0493cc8f89150c8c5966c446c64e6
Dean Siroky <dcsiroky@gmail.com>
2016-08-12 15:37:09 -0400

    Google MapView Added to XML

--- app/src/main/java/com/launchathon/deanc/nap_and_ride/MainActivity.java
+++ app/src/main/java/com/launchathon/deanc/nap_and_ride/MainActivity.java
@@ -1,6 +1,12 @@
 package com.launchathon.deanc.nap_and_ride;
 
+import android.*;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 
@@ -10,21 +16,49 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Bind(R.id.lat) EditText lat;
     @Bind(R.id.lon) EditText lon;
     @Bind(R.id.wakeup_distance) EditText wake_up;
-
-
+    Long userLat, userLon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         ButterKnife.bind(this);
+
+        LocationManager locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, (long) 1000, (float) 10, new LocationListener() {
+
+            @Override
+            public void onLocationChanged(Location location) {
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+
+        });
+        Location location = locationManager.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER);
+        userLat = Long.valueOf((String.valueOf(location.getLatitude())));
+        userLon = Long.valueOf((String.valueOf(location.getLongitude())));
 
 
     }
