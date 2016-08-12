bdfb7088293cc460ddd3b551e0dd16aad815b5bd
emerani <ezraerani@gmail.com>
2016-08-12 16:39:38 -0400

    per elijah's request

--- app/src/main/java/com/launchathon/deanc/nap_and_ride/MainActivity.java
+++ app/src/main/java/com/launchathon/deanc/nap_and_ride/MainActivity.java
@@ -1,69 +1,195 @@
 package com.launchathon.deanc.nap_and_ride;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.util.UUID;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks
+        , GoogleApiClient.OnConnectionFailedListener {
+
+    private final String TAG = MainActivity.class.getName();
+
+    private final int LOCATION_PERMISSION_REQUEST = 1;
 
 
-    @Bind(R.id.lat) EditText lat;
-    @Bind(R.id.lon) EditText lon;
-    @Bind(R.id.wakeup_distance) EditText wake_up;
+    @Bind(R.id.lat)
+    EditText lat;
+    @Bind(R.id.lon)
+    EditText lon;
+    @Bind(R.id.wakeup_distance)
+    EditText wake_up;
     NotificationManager manager;
-
+    GoogleApiClient apiClient;
+    Geofence geofence;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         manager = ((NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE));
 
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-        ButterKnife.bind(this);
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,
+                            android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST);
+        } else {
+            //DO Location stuff
+            if (apiClient == null) {
+                apiClient = new GoogleApiClient.Builder(this)
+                        .addConnectionCallbacks(this)
+                        .addOnConnectionFailedListener(this)
+                        .addApi(LocationServices.API)
+                        .build();
+            }
+        }
 
 
+    }
 
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // You have permission, call your method to do location stuff
+                } else {
+                    // You don't have it, react accordingly
+                }
+        }
     }
 
     @OnClick(R.id.submitButton)
     public void setStop() {
-        Long latitude = Long.valueOf(lat.getText().toString());
-        Long longituge = Long.valueOf(lon.getText().toString());
+        Double latitude = Double.valueOf(lat.getText().toString());
+        Double longituge = Double.valueOf(lon.getText().toString());
         Float radius = Float.valueOf(wake_up.getText().toString());
 
-        Geofence.Builder builder = new Geofence.Builder();
-        builder.setCircularRegion(latitude, longituge, radius)
-                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER);
-        Geofence geofence = builder.build();
-        getGeofencingRequest(geofence);
+        String id = UUID.randomUUID().toString();
 
+        geofence = new Geofence.Builder()
+                .setRequestId(id)
+                .setCircularRegion(latitude, longituge, radius)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER)
+                .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                .build();
+
+        setPerimiter();
 
 
     }
 
-    private GeofencingRequest getGeofencingRequest(Geofence geofence) {
-        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
-        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
-        builder.addGeofence(geofence);
-        return builder.build();
+    private GeofencingRequest getGeofencingRequest() {
+
+        GeofencingRequest request = new GeofencingRequest.Builder()
+                .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
+                .addGeofence(geofence)
+                .build();
+        return request;
+    }
+
+    private PendingIntent getGeofencePendingIntent() {
+        // Reuse the PendingIntent if we already have it.
+//        if (mGeofencePendingIntent != null) {
+//            return mGeofencePendingIntent;
+//        }
+        Intent intent = new Intent(this, GeofenceIntentService.class);
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when
+        // calling addGeofences() and removeGeofences().
+        return PendingIntent.getService(this, 0, intent, PendingIntent.
+                FLAG_UPDATE_CURRENT);
     }
 
     private void triggerNotification() {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
         builder.setContentTitle("arrived");
         manager.notify(0, builder.build());
+    }
+
+    private boolean permissionCheck() {
+        return false;
+    }
+
+    private void setPerimiter() {
+
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            LocationServices.GeofencingApi.addGeofences(
+                    apiClient,
+                    getGeofencingRequest(),
+                    getGeofencePendingIntent()
+            ).setResultCallback((ResultCallback<? super Status>) this);
+            return;
+        }
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (apiClient == null) {
+            apiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+        apiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        apiClient.disconnect();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
